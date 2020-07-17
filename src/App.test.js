a5b8370fd818ddb09d5d2a0e035147063fd03fe3
slroberts <shomariroberts@gmail.comclear>
2020-07-16 23:37:35 -0400

    completed MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {render, fireEvent} from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("test user can submit form successfully", async () => {
+  const {getByTestId} = render(<ContactForm />);
+
+  fireEvent.change(getByTestId("firstName"), {
+    target: {value: "Shomari"},
+  });
+  fireEvent.change(getByTestId("lastName"), {
+    target: {value: "Roberts"},
+  });
+  fireEvent.change(getByTestId("email"), {
+    target: {value: "test@test.com"},
+  });
+  // fireEvent.change(getByTestId("message"), {
+  //   target: {value: "Test message."},
+  // });
+  fireEvent.click(getByTestId("submitButton"));
+});
+
+// test("input reuired to be true", () => {
+//   const {getByTestId} = render(<ContactForm />);
+
+//   let firstName = getByTestId("firstName");
+//   let lastName = getByTestId("lastName");
+//   let email = getByTestId("email");
+//   let message = getByTestId("message");
+//   let notTrue = false;
+//   expect([firstName, lastName, email, message, notTrue]).toBeTruthy();
+// });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
 
 const ContactForm = () => {
   const [data, setData] = useState();
-  const { register, errors, handleSubmit } = useForm({
+  const {register, errors, handleSubmit} = useForm({
     mode: "onBlur",
   });
   const onSubmit = (data) => {
@@ -16,9 +16,10 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            data-testid="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({required: true, maxLength: 3})}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -28,9 +29,10 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            data-testid="lastName"
             name="lastName"
             placeholder="Burke"
-            ref={register({ required: true })}
+            ref={register({required: true})}
           />
           {errors.lastName && (
             <p>Looks like there was an error: {errors.lastName.type}</p>
@@ -41,21 +43,29 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input
+            data-testid="email"
+            name="email"
+            ref={register({required: true})}
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            data-testid="message"
+            name="message"
+            ref={register({required: false})}
+          />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre style={{textAlign: "left", color: "white"}}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submitButton" type="submit" />
       </form>
     </div>
   );
