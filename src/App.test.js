ea2d56090d38392f791af3467c79898fadbb5da3
slroberts <shomariroberts@gmail.comclear>
2020-07-17 13:58:52 -0400

    added a few more test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,25 @@
 import React from "react";
-import {render, fireEvent} from "@testing-library/react";
+import {render, fireEvent, cleanup} from "@testing-library/react";
 import App from "./App";
 import ContactForm from "./components/ContactForm";
+
+afterEach(cleanup);
 
 test("renders App without crashing", () => {
   render(<App />);
 });
 
-test("test user can submit form successfully", async () => {
+test("First name length: test length of first name length is at least 3 characters", () => {
+  const {getByTestId} = render(<ContactForm />);
+
+  let firstname = getByTestId("firstName");
+
+  firstname.value = "Sho";
+
+  expect(firstname.value.length).toBeGreaterThanOrEqual(3);
+});
+
+test("Form can submit : test user can submit form successfully", async () => {
   const {getByTestId} = render(<ContactForm />);
 
   fireEvent.change(getByTestId("firstName"), {
@@ -19,19 +31,35 @@ test("test user can submit form successfully", async () => {
   fireEvent.change(getByTestId("email"), {
     target: {value: "test@test.com"},
   });
-  // fireEvent.change(getByTestId("message"), {
-  //   target: {value: "Test message."},
-  // });
   fireEvent.click(getByTestId("submitButton"));
 });
 
-// test("input reuired to be true", () => {
-//   const {getByTestId} = render(<ContactForm />);
+test("Inputs Required: test that inputs are required", async () => {
+  const {getByTestId} = render(<ContactForm />);
 
-//   let firstName = getByTestId("firstName");
-//   let lastName = getByTestId("lastName");
-//   let email = getByTestId("email");
-//   let message = getByTestId("message");
-//   let notTrue = false;
-//   expect([firstName, lastName, email, message, notTrue]).toBeTruthy();
-// });
+  let firstname = getByTestId("firstName");
+  let lastname = getByTestId("lastName");
+  let email = getByTestId("email");
+
+  fireEvent.change(firstname, {
+    target: {value: "Shomari"},
+  });
+  fireEvent.change(lastname, {
+    target: {value: "Roberts"},
+  });
+  fireEvent.change(email, {
+    target: {value: "test@test.com"},
+  });
+
+  expect(firstname).toHaveValue();
+  expect(lastname).toHaveValue();
+  expect(email).toHaveValue();
+});
+
+test("Messge not required: test that message is not a required field", () => {
+  const {getByTestId} = render(<ContactForm />);
+
+  let message = getByTestId("message");
+
+  expect(message).not.toBeRequired();
+});
